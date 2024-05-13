5912d8be56712fe6b540ea6893bc8ee3e2fb1d06
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-13 16:01:09 +0200

    Updated comment

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -7,7 +7,7 @@ const filename = process.argv[2];
 // get the content to write in the file
 let dataToWrite = process.argv[3];
 
-// open and read the content of the file
+// open and write the content to the file
 fs.writeFile(filename, dataToWrite, 'utf8', (err) => {
   if (err) {
     console.log(err);

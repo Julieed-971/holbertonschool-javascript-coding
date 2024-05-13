6c75e06ae29a32aaf104b5512548532c0caa18b4
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-13 16:20:02 +0200

    Changed variable type

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const filename = process.argv[2];
 
 // get the content to write in the file
-let dataToWrite = process.argv[3];
+const dataToWrite = process.argv[3];
 
 // open and write the content to the file
 fs.writeFile(filename, dataToWrite, 'utf8', (err) => {

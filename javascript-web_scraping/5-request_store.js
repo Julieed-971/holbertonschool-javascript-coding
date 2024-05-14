10deaa9ccad7fb43422d89dfd9a9e5af4bb57b20
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-14 16:51:46 +0200

    Loripsum

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// import filesystem module
+const fs = require('fs');
+
+// import request module
+const request = require('request');
+
+// get the URL to copy text from
+const textToCopyUrl = process.argv[2];
+
+// get the filename to copy the text to
+const fileName = process.argv[3];
+
+// make a get request to the provided url 
+request.get(textToCopyUrl, (error, response, body) => {
+	if (error) {
+		console.log(error);
+	}
+
+	// write the text from the URL to the specified file
+	fs.writeFile(fileName, body, 'utf8', (err) => {
+		if (err) {
+			console.log(err);
+		}
+	});
+});

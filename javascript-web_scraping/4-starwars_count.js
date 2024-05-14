3e3535e8327e2e87f9ca06410cf9b1b6620a1d4a
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-14 15:37:17 +0200

    Removed extra array type check condition

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,29 +9,23 @@ const url = process.argv[2];
 request.get(url, (error, response, body) => {
   if (error) {
     console.error(error);
-    return;
   }
 
   // parse response body as JSON
   const movieData = JSON.parse(body);
+  let count = 0;
 
-  // check if the response contains a 'results' array
-  if (Array.isArray(movieData.results)) {
-    let count = 0;
+  // iterate over each movie object
+  for (const movie of movieData.results) {
 
-    // iterate over each movie object
-    for (const movie of movieData.results) {
+    // iterate over the "characters" array of current movie
+    for (const characterUrl of movie.characters) {
 
-      // iterate over the "characters" array of current movie
-      for (const characterUrl of movie.characters) {
-
-        // check if characters array contains characterUrl
-        if (characterUrl.includes('18')) {
-          count++;
-          break;
-        }
+      // check if characters array contains characterUrl
+      if (characterUrl.includes('18')) {
+        count++;
       }
     }
-    console.log(count);
   }
+  console.log(count);
 });

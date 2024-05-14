8f0e305dbdf6edf2f16eff8fdc2fdd7d7b65fb03
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-14 15:56:41 +0200

    Put semicolons back

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,29 +1,26 @@
 #!/usr/bin/node
 /* Script that prints the number of movies where the character “Wedge Antilles” is present */
-const request = require('request')
-
+const request = require('request');
 // get the url to request
-const url = process.argv[2]
+const url = process.argv[2];
 
 // calling the request get method
-request.get(url, (error, _response, body) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error)
+    console.error(error);
   }
-
   // parse response body as JSON
-  const movieData = JSON.parse(body)
-  let count = 0
-
+  const movieData = JSON.parse(body);
+  let count = 0;
   // iterate over each movie object
   for (const movie of movieData.results) {
     // iterate over the "characters" array of current movie
     for (const characterUrl of movie.characters) {
       // check if characters array contains characterUrl
       if (characterUrl.includes('18')) {
-        count++
+        count++;
       }
     }
   }
-  console.log(count)
+  console.log(count);
 })

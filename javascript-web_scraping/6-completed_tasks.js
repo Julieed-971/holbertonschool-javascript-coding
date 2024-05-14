50a580c60b851b845d48027f7b3735c8d0b78828
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-14 18:07:37 +0200

    How many completed?

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,40 @@
+#!/usr/bin/node
+// import request module
+const request = require('request');
+
+// get the URL to process data from
+const url = process.argv[2];
+
+// make a get request to the provided urel
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+
+  // parse the json string into an object
+  const todosData = JSON.parse(body);
+  let completedTasks = {};
+
+  // iterate through the data
+  for (const task of todosData) {
+
+    // get the userId
+    const	userId = task.userId;
+
+    // get the boolean value of the task completion status
+    const isCompleted = task.completed;
+
+      // initialize the object for the current userId 
+      if (!completedTasks[userId]) {
+        completedTasks[userId] = 0;
+      }
+
+      // if completed is true, increment the number of completed task
+      if (isCompleted) {
+        completedTasks[userId]++;
+      }
+    }
+
+  // print the results
+  console.log(completedTasks);
+});

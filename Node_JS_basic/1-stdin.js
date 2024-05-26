1d4671bcc298c7504742a313c4e9cf8ba8fb336c
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-26 21:05:41 +0200

    Used process to write instead of console log

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,20 +4,18 @@ process.stdin.setEncoding('utf-8');
 // Prompt the user for their name
 console.log('Welcome to Holberton School, what is your name?');
 
-// Listen for data from the standard input (stdin) once
-process.stdin.once('data', (data) => {
-  // Convert the input data to a string and remove any leading/trailing whitespace
-  const input = data.trim();
+// Listen for data from the standard input (stdin) on
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
 
-  // Log the input data to the console
-  console.log(`Your name is: ${input}`);
-
+  // Check if input is not null
+  if (input !== null) {
+    // Write the name in the standard output
+    process.stdout.write(`Your name is: ${input}`);
+  }
   // Notify the user that the software is closing
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 
   // Exit the process
   process.exit(0);
 });
-
-// Resume the stdin stream to start receiving data
-process.stdin.resume();

04960343173a8f3ae8847ce7fa9c01df0baed7c1
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-31 14:06:32 +0200

    Fixed code according to requirements

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,12 @@
 //  create a small HTTP server using the http module
 const http = require('http');
 
-const server = http.createServer((request, response) => {
+const app = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-type': 'text/plain' });
   response.write('Hello Holberton School!');
   response.end();
 });
 
-server.listen(1245);
+app.listen(1245);
+
+module.exports = app;

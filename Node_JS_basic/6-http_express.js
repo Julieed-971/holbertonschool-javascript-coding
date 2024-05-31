1c58f6b638f9837e96fc05c382992c47a0895dac
Julieed-971 <julie.dedieu1@gmail.com>
2024-05-31 19:49:40 +0200

    Create a small HTTP server using Express

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (request, response) => {
+  response.setHeader('Content-Type', 'text/plain');
+  response.status(200)
+    .send('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
